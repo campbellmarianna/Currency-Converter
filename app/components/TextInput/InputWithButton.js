9cc9a6cc6a89b6e579d00b4930128826e4726121
Marianna <mckd2018@gmail.com>
2019-09-08 16:13:46 -0700

    [add] reverse currencies button to home screen

--- app/components/TextInput/InputWithButton.js
+++ app/components/TextInput/InputWithButton.js
@@ -7,31 +7,33 @@ import color from 'color';
 
 import styles from './styles';
 
-const InputWithButton = (props) => {
-  const {
-    onPress,
-    buttonText,
-    editable = true,
-  } = props;
-
-  const underlayColor = color(styles.$buttonBackgroundColorBase).darken(styles.$buttonBackgroundColorModifier, 
+// How does this work???
+const InputWithButton = ({
+  editable,
+  onPress,
+  buttonText,
+  ...props
+}) => {
+  const underlayColor = color(styles.$buttonBackgroundColorBase).darken(
+    styles.$buttonBackgroundColorModifier,
   );
 
   const containerStyles = [styles.container];
   if (editable === false) {
     containerStyles.push(styles.containerDisabled);
   }
+
   return (
     <View style={containerStyles}>
       <TouchableHighlight
-        underlayColor={underlayColor}
-        style={styles.buttonContainer}
         onPress={onPress}
+        style={styles.buttonContainer}
+        underlayColor={underlayColor}
       >
         <Text style={styles.buttonText}>{buttonText}</Text>
       </TouchableHighlight>
-      <View style={styles.border} />
-      <TextInput style={styles.input} />
+      <View style={styles.separator} />
+      <TextInput style={styles.input} underlineColorAndroid="transparent" {...props} />
     </View>
   );
 };
